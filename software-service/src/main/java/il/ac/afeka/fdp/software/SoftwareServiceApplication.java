7114512452829ce807f122f602f730d90f2369dc
Liad Khamdadash <liadkh95@gmail.com>
2020-05-19 17:46:59 +0300

    add software service

---
+++ software-service/src/main/java/il/ac/afeka/fdp/software/SoftwareServiceApplication.java
@@ -0,0 +1,13 @@
+package il.ac.afeka.fdp.software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareServiceApplication.class, args);
+	}
+
+}
