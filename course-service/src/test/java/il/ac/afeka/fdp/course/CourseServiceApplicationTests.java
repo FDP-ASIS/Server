571155e3e9c1059f8fb4370460c6d8d900da5d9e
Liad Khamdadash <liadkh95@gmail.com>
2020-05-19 17:43:21 +0300

    add course service

---
+++ course-service/src/test/java/il/ac/afeka/fdp/course/CourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package il.ac.afeka.fdp.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
