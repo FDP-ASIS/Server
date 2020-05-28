20ef0b04fde97b20d3fe87a38df5c228408ed2f2
Slava Kagan <slava15.92@gmail.com>
2020-05-28 14:18:06 +0300

    Implement software service

---
+++ software-service/src/main/java/il/ac/afeka/fdp/software/crud/SoftwareCrud.java
@@ -0,0 +1,15 @@
+package il.ac.afeka.fdp.software.crud;
+
+import il.ac.afeka.fdp.software.model.Software;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SoftwareCrud extends MongoRepository<Software, String> {
+    boolean existsSoftwareBySoftwareName(String softwareName);
+    Software findBySoftwareName(@Param("softwareName") String softwareName);
+    List<Software> findAllBySoftwareName(@Param("softwareName") String softwareName, Pageable pageable);
+    List<Software> findAllByVersion(@Param("software") String version, Pageable pageable);
+}
